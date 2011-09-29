30d9955e6dff64d2c9382f8c3f416010be316d2f
sbrissen <sbrissen@Hotmail.com>
2011-09-29 15:42:31 -0400

    RingSelector widget commits from CM, failed custom touchkeytime

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -98,7 +98,7 @@ class PowerManagerService extends IPowerManager.Stub
     // The short keylight delay comes from secure settings; this is the default.
     private static final int SHORT_KEYLIGHT_DELAY_DEFAULT = 6000; // t+6 sec
     private static final int MEDIUM_KEYLIGHT_DELAY = 15000;       // t+15 sec
-    private static final int LONG_KEYLIGHT_DELAY = 6000;        // t+6 sec
+    private static final int LONG_KEYLIGHT_DELAY = 30000;        // t+6 sec
     private static final int LONG_DIM_TIME = 7000;              // t+N-5 sec
 
     // How long to wait to debounce light sensor changes.
@@ -111,7 +111,7 @@ class PowerManagerService extends IPowerManager.Stub
     private static final float PROXIMITY_THRESHOLD = 5.0f;
 
     // Cached secure settings; see updateSettingsValues()
-    private int mShortKeylightDelay = SHORT_KEYLIGHT_DELAY_DEFAULT;
+    private int mShortKeylightDelay; //= Settings.System.getInt(mContext.getContentResolver(),Settings.System.KEYLIGHT_TIMEOUT,SHORT_KEYLIGHT_DELAY_DEFAULT);
 
     // Default timeout for screen off, if not found in settings database = 15 seconds.
     private static final int DEFAULT_SCREEN_OFF_TIMEOUT = 15000;
@@ -639,6 +639,7 @@ class PowerManagerService extends IPowerManager.Stub
                     updateSettingsValues();
                 }
             });
+
         updateSettingsValues();
 
         synchronized (mHandlerThread) {
@@ -1201,7 +1202,7 @@ class PowerManagerService extends IPowerManager.Stub
                     switch (nextState)
                     {
                         case SCREEN_BRIGHT:
-                            when = now + mKeylightDelay;
+			    when = now + mKeylightDelay;
                             break;
                         case SCREEN_DIM:
                             if (mDimDelay >= 0) {
@@ -2593,21 +2594,28 @@ class PowerManagerService extends IPowerManager.Stub
                 mContext.getContentResolver(),
                 Settings.System.KEYLIGHT_TIMEOUT,
                 SHORT_KEYLIGHT_DELAY_DEFAULT);
+	mKeylightDelay = mShortKeylightDelay;
 
         if ((mPokey & POKE_LOCK_SHORT_TIMEOUT) != 0) {
             mKeylightDelay = mShortKeylightDelay;  // Configurable via secure settings
             mDimDelay = -1;
             mScreenOffDelay = 0;
         } else if ((mPokey & POKE_LOCK_MEDIUM_TIMEOUT) != 0) {
-            mKeylightDelay = MEDIUM_KEYLIGHT_DELAY;
+            mKeylightDelay = 0x3a98;
+	    //mKeylightDelay = MEDIUM_KEYLIGHT_DELAY;
             mDimDelay = -1;
             mScreenOffDelay = 0;
+	}else if ((mPokey & 0x10) != 0){
+	    mKeylightDelay = mShortKeylightDelay;
+	    mDimDelay = -1;
+	    mScreenOffDelay = 0x4e20;
         } else {
             int totalDelay = mScreenOffTimeoutSetting;
             if (totalDelay > mMaximumScreenOffTimeout) {
                 totalDelay = mMaximumScreenOffTimeout;
             }
-            mKeylightDelay = LONG_KEYLIGHT_DELAY;
+            mKeylightDelay = mShortKeylightDelay;
+	    //mKeylightDelay = LONG_KEYLIGHT_DELAY;
             if (totalDelay < 0) {
                 mScreenOffDelay = Integer.MAX_VALUE;
             } else if (mKeylightDelay < totalDelay) {
@@ -2625,11 +2633,11 @@ class PowerManagerService extends IPowerManager.Stub
                 mDimDelay = -1;
             }
         }
-        if (mSpew) {
+       // if (mSpew) {
             Slog.d(TAG, "setScreenOffTimeouts mKeylightDelay=" + mKeylightDelay
                     + " mDimDelay=" + mDimDelay + " mScreenOffDelay=" + mScreenOffDelay
                     + " mDimScreen=" + mDimScreen);
-        }
+       // }
     }
 
     /**
@@ -2637,12 +2645,26 @@ class PowerManagerService extends IPowerManager.Stub
      * on subsequent changes to secure settings.
      */
     private void updateSettingsValues() {
+<<<<<<< HEAD
 	
         mShortKeylightDelay = Settings.Secure.getInt(
                 mContext.getContentResolver(),
                 Settings.Secure.SHORT_KEYLIGHT_DELAY_MS,
                 SHORT_KEYLIGHT_DELAY_DEFAULT);
         // Slog.i(TAG, "updateSettingsValues(): mShortKeylightDelay now " + mShortKeylightDelay);
+=======
+
+        /*mShortKeylightDelay = Settings.Secure.getInt(
+                mContext.getContentResolver(),
+                Settings.Secure.SHORT_KEYLIGHT_DELAY_MS,
+                SHORT_KEYLIGHT_DELAY_DEFAULT); */
+        mShortKeylightDelay = Settings.System.getInt(
+                mContext.getContentResolver(),
+                Settings.System.KEYLIGHT_TIMEOUT,
+                SHORT_KEYLIGHT_DELAY_DEFAULT);
+	
+         Slog.i(TAG, "updateSettingsValues(): mShortKeylightDelay now " + mShortKeylightDelay);
+>>>>>>> ca969f3... RingSelector widget commits from CM, failed custom touchkeytime
     }
 
     private class LockList extends ArrayList<WakeLock>
